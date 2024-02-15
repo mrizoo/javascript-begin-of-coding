55d371d33eee02e5005559a9995f3b163f7adaaf
mukhammadrizo <pulatovvmukhammadrizo@gmail.com>
2024-02-15 14:35:35 +0500

    coding aboutb javascript

---
+++ app.js
@@ -0,0 +1,27 @@
+    const yosh = '23'; // string<
+    console.log(yosh); // string<
+    console.log( Number(yosh)); // Number<
+
+
+    console.log(yosh+1); // 231 (concantenation)<
+    console.log(Number(yosh)+1); // 24<
+
+
+    const ism = 'Muhammadrizo';
+    console.log(Number(ism)); // Nan<
+
+
+
+    const yil = '2024'; // Number<
+    console.log(yil); // Number<
+    console.log(String(yil)); // String<
+
+    console.log('30' - '10' - 5 );
+
+
+    const name = '' ;
+    if (ism) { 
+    console.log('Sizning ismingiz ' +  ism);
+    } else {
+     console.log ('Iltimos ism kiriting');
+    }
